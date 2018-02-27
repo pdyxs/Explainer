eedc622ae99006676aa68ea82f461745ad325df7
Paul Sztajer <paul@sztajer.org>
2018-02-27 17:46:56 +0800

    remove reliance on skins

--- FocusOnDraggable.cs
+++ FocusOnDraggable.cs
@@ -2,19 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using PDYXS.Skins;
+//using PDYXS.Skins;
 
 namespace PDYXS.Explainer
 {
     [RequireComponent(typeof(Draggable))]
     public class FocusOnDraggable : MonoBehaviour
     {
-        private SkinnedObjectProvider<Focusable> _focusable;
-        public Focusable focusable {
-            get {
-                return _focusable.obj;
-            }
-        }
+        //private SkinnedObjectProvider<Focusable> _focusable;
+        //public Focusable focusable {
+        //    get {
+        //        return _focusable.obj;
+        //    }
+        //}
 
         private Draggable _draggable;
         public Draggable draggable {
@@ -28,13 +28,13 @@ namespace PDYXS.Explainer
 
         private void Start()
         {
-            _focusable = this.Provide<Focusable>();
+            //_focusable = this.Provide<Focusable>();
             draggable.OnDragBegun.AddListener(OnDragBegun);
         }
 
         private void OnDragBegun(Draggable arg0)
         {
-            focusable.Focus();
+            //focusable.Focus();
         }
     }
 }
